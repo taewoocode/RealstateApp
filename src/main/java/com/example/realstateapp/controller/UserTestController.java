c99c42ea5434ad2a29868a6025ec99599b1f74b8
taewoocode <qkrxodn6035@gmail.com>
2024-10-21 21:30:58 +0900

    mysql 의존성 추가

--- src/main/java/com/example/realstateapp/controller/UserTestController.java
+++ src/main/java/com/example/realstateapp/controller/UserTestController.java
@@ -5,12 +5,14 @@ import com.example.realstateapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/users")
 @Controller
 public class UserTestController {
-
 
     private final UserService userService;
 
@@ -19,8 +21,21 @@ public class UserTestController {
         this.userService = userService;
     }
 
+    @GetMapping("/register")
     public String showRegistration(Model model) {
         model.addAttribute( "user", new User() );
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user) {
+        userService.createUser( user );
+        return "redirect:/users/register?success";
+    }
+
+    @GetMapping("/success")
+    public String registrationSuccess(Model model) {
+        model.addAttribute( "meesage", "회원가입 성공" );
         return "register";
     }
 }
