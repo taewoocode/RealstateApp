b517cc6e6ab829aab79bd1d7add73e1cc84e2f63
taewoocode <qkrxodn6035@gmail.com>
2024-10-22 00:53:19 +0900

    카카오 맵 서비스 구현

--- src/main/java/com/example/realstateapp/service/KakaoMapService.java
+++ src/main/java/com/example/realstateapp/service/KakaoMapService.java
@@ -2,8 +2,13 @@ package com.example.realstateapp.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class KakaoMapService {
@@ -17,5 +22,26 @@ public class KakaoMapService {
     public KakaoMapService(RestTemplate restTemplate, ObjectMapper objectMapper) {
         this.restTemplate = restTemplate;
         this.objectMapper = objectMapper;
+    }
+    public String getLocationInfo(String query) {
+        // 카카오맵 API URL
+        String url = "https://dapi.kakao.com/v2/local/search/keyword.json";
+
+        // URI 구성
+        String requestUrl = UriComponentsBuilder.fromHttpUrl(url)
+                .queryParam("query", query)
+                .build()
+                .toUriString();
+
+        // HTTP 헤더에 API 키 추가
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "KakaoAK " + kakaoApiKey);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        // API 호출
+        ResponseEntity<String> response = restTemplate.exchange(requestUrl, HttpMethod.GET, entity, String.class);
+
+        // 결과 반환
+        return response.getBody();
     }
 }
