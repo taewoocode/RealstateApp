d3e872f5832dfeab778307dff41ec78192cc8827
taewoocode <qkrxodn6035@gmail.com>
2024-10-21 21:18:01 +0900

    등록페이지 구현 및 컨트롤러 작성

--- src/main/java/com/example/realstateapp/controller/UserTestController.java
+++ src/main/java/com/example/realstateapp/controller/UserTestController.java
@@ -1,4 +1,26 @@
 package com.example.realstateapp.controller;
 
+import com.example.realstateapp.model.User;
+import com.example.realstateapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/users")
+@Controller
 public class UserTestController {
+
+
+    private final UserService userService;
+
+    @Autowired
+    public UserTestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    public String showRegistration(Model model) {
+        model.addAttribute( "user", new User() );
+        return "register";
+    }
 }
